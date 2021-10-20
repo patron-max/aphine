cd4bd781aaa8b0033ed047971c5c3ae335a0a710
Iurii Cazarean <cazaream@yahoo.com>
2021-10-20 11:08:21 +0300

    change setting

--- aphina/settings.py
+++ aphina/settings.py
@@ -73,22 +73,22 @@ WSGI_APPLICATION = 'aphina.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgressql_psycopg2',
-        'NAME': 'prod_db',
-        'USER': 'admin',
-        'PASSWORD': 'devpass',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgressql_psycopg2',
+#         'NAME': 'prod_db',
+#         'USER': 'admin',
+#         'PASSWORD': 'devpass',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
