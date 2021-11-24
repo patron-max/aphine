f3536a35bc0f34cf15c1a5ee6522d2e5cd480785
Iurii <cazarean@yahoo.com>
2021-11-24 16:44:02 +0200

    add nav bar

--- covidlab/views.py
+++ covidlab/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+# def index(request):
+#     # print(dir(request))
+#     return HttpResponse('Hello world')
 
 
 def lab(request):
     return HttpResponse('<h1>Тестовая страница</h1>')
+
+
+
+
+
+def index(request):
+    # news = News.objects.all()
+    # context = {
+    #     'news': news,
+    #     'title': 'Список новостей'
+    # }
+    # return render(request, template_name='news/index.html', context=context)
+    return render(request, template_name='covidlab/index.html')
